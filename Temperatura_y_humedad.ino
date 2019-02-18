d71d84ab6c225e29860c436a7442e29e5d1d1204
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2019-02-17 23:57:41 -0500

    Update Temperatura_y_humedad.ino

--- Temperatura_y_humedad.ino
+++ Temperatura_y_humedad.ino
@@ -1,43 +1,64 @@
 #include "ThingSpeak.h"
 #include <ESP8266WiFi.h>
-#include <SPI.h>
-#include "DHT.h" //cargamos la librería DHT
+//#include <WiFi.h>
+#include "DHT.h" 
 #include <Adafruit_Sensor.h>
-#define DHTPIN 2 //Seleccionamos el pin en el que se conectará el sensor
-#define DHTTYPE DHT22 //Se selecciona el DHT22(hay otros DHT)
+#define DHTPIN 2 
+#define DHTTYPE DHT11 
+DHT dht(DHTPIN, DHTTYPE); 
 
-DHT dht(DHTPIN, DHTTYPE); //Se inicia una variable que será usada por Arduino para comunicarse con el sensor   
-    
-    char ssid[] = "XXXXXXXX";   
-    char pass[] = "XXXXXXXX";   
-    int status = WL_IDLE_STATUS;
-    WiFiClient  client; 
-    
-unsigned long myChannelNumber = "Chanel ID"; 
+char ssid[] = "XXXXXXXXX";
+char pass[] = "XXXXXXXXX";
+int status = WL_IDLE_STATUS;
+WiFiClient  client;
+
+unsigned long myChannelNumber = XXXXXXXX;
 const char * myWriteAPIKey = "APIKEY";
-int sensor = 0;
-
-void setup() {  
-Serial.begin(115200);
-dht.begin(); 
-WiFi.begin(ssid, pass);
+void setup() {
+WiFi.begin(ssid, pass); 
 ThingSpeak.begin(client);
+dht.begin();
 
-while( WiFi.status() != WL_CONNECTED){
+Serial.begin(115200);
+Serial.println();
+Serial.print("Conectando a ");
+Serial.print(ssid);
+
+while (WiFi.status() != WL_CONNECTED)
+{
 delay(500);
 Serial.print(".");
 }
-Serial.println("");
-Serial.println("Wi-Fi connectado");
+
+Serial.println();
+Serial.println("Conectado a WiFi");
+Serial.print("Dirección IP: ");
+Serial.println(WiFi.localIP());
 
 }
+
 void loop() {
-float t = dht.readTemperature(); 
+delay(2000);
+
 float h = dht.readHumidity();
+float t = dht.readTemperature();
 
-ThingSpeak.writeField(myChannelNumber,1,t, myWriteAPIKey);
-delay(5000)
-ThingSpeak.writeField(myChannelNumber,2,h, myWriteAPIKey);
-delay (2000);
+Serial.print("Humedad: ");
+Serial.print(h);
+Serial.print(" %\t");
+Serial.print("Temperatura: ");
+Serial.print(t);
+Serial.println(" *C\t ");
 
+// Carga los valores a enviar
+ThingSpeak.setField(1, t);
+
+// Escribe todos los campos a la vez.
+ThingSpeak.writeFields(myChannelNumber, myWriteAPIKey);
+
+Serial.println("¡Datos enviados a ThingSpeak!");
+
+// Añadimos un retraso para limtitar el número de escrituras en Thinhspeak
+
+delay (1000);
 }
