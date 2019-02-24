d86b24feb72105886e57aadd4bef1aa4f1bf2c23
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2019-02-24 16:13:01 -0500

    Update temperatura.py

--- temperatura.py
+++ temperatura.py
@@ -1,25 +1,18 @@
-
 import Adafruit_DHT
 import urllib
 import time
 
 sensor = Adafruit_DHT.DHT22
 
+pin = 23
 
-pin = "P8_11"
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while True: 
+	humedad, temperatura = Adafruit_DHT.read_retry(sensor, pin)
+	if humedad is not None and temperatura is not None:
+		print('Temperatura={0:0.1f}*C  Humedad={1:0.1f}%'.format(temperatura,humedad))
+	else:
+		print('Fallo la lectura del sensor.Intentar de nuevo')
 
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
+	time.sleep(5) 
 
-baseUrl="https://api.thingspeak.com/update?api_key=APIKEY"
-while True:
-  
-  f=urllib.urlopen(baseUrl+"&field1=%s&field2=%s"%(temperature,humidity))
-  print f.read()
-  print baseUrl+"&field1=%s&field2=%s"%(temperature,humidity) 
-  f.close()
 
-time.sleep(25) 
