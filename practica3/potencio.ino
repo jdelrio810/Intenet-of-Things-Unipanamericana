895dc6f84dbc102b218053df5ccc0c44b24e7e83
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2019-03-17 22:27:11 -0500

    Update potencio.ino

--- practica3/potencio.ino
+++ practica3/potencio.ino
@@ -10,23 +10,14 @@ SoftwareSerial ser(2,3);//Rx Tx
 void setup() {
   
   pinMode(sensorPin,INPUT);
-  
   Serial.begin(9600);
-
   ser.begin(9600);
-
-
 }
 
 void loop() {
   
-  sensorValue = (5.0*analogRead(sensorPin))/1023;
-  
+  sensorValue = (3.3*analogRead(sensorPin))/1023;
   ser.println(sensorValue);
-  
   Serial.println(sensorValue);
-  
   delay(1000);
-
-
 }
