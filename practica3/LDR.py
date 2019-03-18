3e8c0dc30192cdbbe76e70a3e74a584a31c1da8a
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2019-03-17 22:54:16 -0500

    Update LDR.py

--- practica3/LDR.py
+++ practica3/LDR.py
@@ -1,11 +1,9 @@
 import serial
-import sys 
+import time
 
-# make sure to use the correct serial serial communication device name 
-# e.g. /dev/ttyUSB0, /dev/ttyUSB1, /dev/ttyACM0, ...
-#  as you discovered in the steps above
-# dmesg | grep -v disconnect | grep -Eo "tty(ACM|USB)." | tail -1
-ser = serial.Serial('/dev/ttyUSB0', 115200)
+
+ser = serial.Serial('/dev/ttyS1', 115200)
+
 
 # loop until manually stopped
 # first flush possibly existing data in the input buffer:
@@ -13,6 +11,7 @@ ser.flushInput()
 while True:
     try:
         # read a single line from the serial interface represented by the ser object
+        ser = serial.Serial('/dev/ttyS1', 115200)
         lineBytes = ser.readline()
         # convert Bytes returned by the ser.readline() function to String
         line = lineBytes.decode('utf-8')
@@ -20,4 +19,4 @@ while True:
         print(line)
         
     except KeyboardInterrupt:
-        break # stop the while loop
+    	 break # stop the while loop
