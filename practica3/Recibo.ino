36c5c3f7286b5ca74e6c213e2369cda51dbee6ef
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2019-03-30 10:15:01 -0500

    Update Recibo.ino

--- practica3/Recibo.ino
+++ practica3/Recibo.ino
@@ -1,17 +1,17 @@
 int led = 5;//pin D1
 
 void setup () {
-      Serial.begin(115200); //Inicializo el puerto serial a 9600 baudios
+      Serial.begin(115200); //Inicializo el puerto serial a 115200 baudios
       pinMode( led , OUTPUT);
 }
 void loop () {
       if (Serial.available()) { //Si está disponible
             char c = Serial.read(); //Guardamos la lectura en una variable char
-            if (c == 'N') { 
+            if (c == 'N') { //Si es una 'N', enciendo el LED
                   digitalWrite(led,HIGH);
-                  
-            } else if (c == 'D') { 
-                  
+                  Serial.println("'N' pulsada");
+            } else if (c == 'D') { //Si es una 'D', apago el LED
+                  Serial.println("'D' pressed");
                   digitalWrite(led,LOW);
             } 
       }
