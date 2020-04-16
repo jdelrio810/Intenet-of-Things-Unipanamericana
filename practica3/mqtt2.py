2df87ede66004bbe0d725e3ce6ac746cdbd80bc7
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2020-04-16 17:21:49 -0500

    Update mqtt2.py

--- practica3/mqtt2.py
+++ practica3/mqtt2.py
@@ -1,26 +1,28 @@
 import paho.mqtt.client as mqtt
-from threading import Thread
 
-print("Bienvenidos al chat")
+
 nombre = input("Hola dime tu nombre: ")
 nombre = nombre.capitalize()
 
-def ante_conexion_exitosa(client,userdata,flags,rc):
+def on_connect(client,userdata,flags,rc):
     print("Conectados con exito")
-    client.subscribe("l4GFVje7zy7jB1Y")
-    client.publish("l4GFVje7zy7jB1Y",nombre+"se ha sumado al chat")
+    client.subscribe("topic")
+    client.publish("topic",nombre)
 
-def ante_llegada_mensaje(client,userdata,msg):
+def on_message(client,userdata,msg):
     print(msg.payload.decode("utf-8"))
+    
 cliente = mqtt.Client()
-cliente.on_connect = ante_conexion_exitosa
-cliente.on_message = ante_llegada_mensaje
-cliente.username_pw_set("48hsYf7kthMm3Sr","Tv7qInpl3cQA0SR")
+cliente.on_connect = on_connect
+cliente.on_message = on_message
+cliente.username_pw_set("usuario","contraseña")
 cliente.connect("ioticos.org",1883,60)
+
+
 while True:
     a_enviar=input("escribe un mensaje ->")
     a_envir = nombre + "dice"+ a_enviar
-    cliente.publish("l4GFVje7zy7jB1Y",a_enviar)
+    cliente.publish("topic",a_enviar)
     cliente.loop()
 
 
