1d9cb2a7681e64c571b5d7fd71f0e53331fbd6a9
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2020-04-22 22:56:36 -0500

    Update mqtt2.py

--- practica3/mqtt2.py
+++ practica3/mqtt2.py
@@ -1,28 +1,24 @@
 import paho.mqtt.client as mqtt
 
 
-nombre = input("Hola dime tu nombre: ")
-nombre = nombre.capitalize()
-
 def on_connect(client,userdata,flags,rc):
     print("Conectados con exito")
     client.subscribe("topic")
-    client.publish("topic",nombre)
 
 def on_message(client,userdata,msg):
     print(msg.payload.decode("utf-8"))
     
+    
 cliente = mqtt.Client()
 cliente.on_connect = on_connect
 cliente.on_message = on_message
-cliente.username_pw_set("usuario","contraseña")
-cliente.connect("ioticos.org",1883,60)
+#cliente.username_pw_set("usuario","contraseña")
+cliente.connect("Servidor",1883,60)
 
 
 while True:
+    
     a_enviar=input("escribe un mensaje ->")
-    a_envir = nombre + "dice"+ a_enviar
     cliente.publish("topic",a_enviar)
     cliente.loop()
-
 
