06a7f3b3becbcdfb1e820dfcac588c121d41565b
jdelrio810 <31167486+jdelrio810@users.noreply.github.com>
2020-05-04 22:35:30 -0500

    Update blynk.py

--- practica 4/blynk.py
+++ practica 4/blynk.py
@@ -1,19 +1,21 @@
 
 
 import blynklib
+import random
 
 BLYNK_AUTH = 'YourAuthToken'
 
-# initialize Blynk
+# initialize blynk
 blynk = blynklib.Blynk(BLYNK_AUTH)
 
-WRITE_EVENT_PRINT_MSG = "[WRITE_VIRTUAL_PIN_EVENT] Pin: V{} Value: '{}'"
+READ_PRINT_MSG = "[READ_VIRTUAL_PIN_EVENT] Pin: V{}"
 
 
-# register handler for virtual pin V4 write event
-@blynk.handle_event('write V4')
-def write_virtual_pin_handler(pin, value):
-    print(WRITE_EVENT_PRINT_MSG.format(pin, value))
+# register handler for virtual pin V11 reading
+@blynk.handle_event('read V11')
+def read_virtual_pin_handler(pin):
+    print(READ_PRINT_MSG.format(pin))
+    blynk.virtual_write(pin, random.randint(0, 255))
 
 
 ###########################################################
